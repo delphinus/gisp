2cb0f07cdcabb267f3577667458de93e4d1ae40d
delphinus <me@delphinus.dev>
2024-01-21 21:11:14 +0900

    fix: show valid version from formula

--- gisp.rb
+++ gisp.rb
@@ -4,9 +4,9 @@
 class Gisp < Formula
   desc 'Google IME for SKK'
   homepage 'https://github.com/delphinus/homebrew-gisp'
-  url 'https://github.com/delphinus/homebrew-gisp/archive/v0.0.4.tar.gz'
-  sha256 '2648b7c07089454ef19437a351667183dd231b1b89a8bee1c760af877bfdb3ee'
-  version '0.0.4'
+  url 'https://github.com/delphinus/homebrew-gisp/archive/v0.0.5.tar.gz'
+  sha256 'fd84badf0350c572d54c797136baea54c5ed1c64fd409dc8d82a606d80a47149'
+  version '0.0.5'
   head 'https://github.com/delphinus/homebrew-gisp.git'
 
   service do
@@ -17,6 +17,7 @@ class Gisp < Formula
   end
 
   def install
+    inreplace 'gisp', /(?<=our \$VERSION = ').*(?=';)/, version.to_s
     bin.install 'gisp'
   end
 end
