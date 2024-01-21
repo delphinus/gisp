1d2179d5d5853421bac59a46359fe47d7e78beb8
delphinus <me@delphinus.dev>
2024-01-21 18:34:57 +0900

    feat: add Homebrew Services feature

--- gisp.rb
+++ gisp.rb
@@ -4,10 +4,17 @@
 class Gisp < Formula
   desc 'Google IME for SKK'
   homepage 'https://github.com/delphinus/homebrew-gisp'
-  url 'https://github.com/delphinus/homebrew-gisp/archive/v0.0.3.tar.gz'
-  sha256 'b4b91a8359c9fa7412b8688ef9dd2ceb645cb0cadb0f9a8bf952e093665d268c'
-  version '0.0.3'
+  url 'https://github.com/delphinus/homebrew-gisp/archive/v0.0.4.tar.gz'
+  sha256 '2648b7c07089454ef19437a351667183dd231b1b89a8bee1c760af877bfdb3ee'
+  version '0.0.4'
   head 'https://github.com/delphinus/homebrew-gisp.git'
+
+  service do
+    run bin / 'gisp', '-v'
+    keeyalive true
+    log_path "#{Dir.home}/Library/Logs/gisp.log"
+    error_log_path "#{Dir.home}/Library/Logs/gisp_error.log"
+  end
 
   def install
     bin.install 'gisp'
