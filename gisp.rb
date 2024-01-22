b84d62c95d89d131ff315a9003b78f28648effc2
delphinus <me@delphinus.dev>
2024-01-22 15:31:30 +0900

    feat: bump version up

--- gisp.rb
+++ gisp.rb
@@ -4,9 +4,9 @@
 class Gisp < Formula
   desc 'Google IME for SKK'
   homepage 'https://github.com/delphinus/homebrew-gisp'
-  url 'https://github.com/delphinus/homebrew-gisp/archive/v1.0.4.tar.gz'
-  sha256 '823929036c834676d8b03797e6d2b6444cc85659f1d7aa801ae0a1245ba78fda'
-  version '1.0.4'
+  url 'https://github.com/delphinus/homebrew-gisp/archive/v1.0.5.tar.gz'
+  sha256 '554dad43724dea4c8a93682e03e34cd366d951b5d3d23b1bed62e93d540f85ab'
+  version '1.0.5'
   head 'https://github.com/delphinus/homebrew-gisp.git'
 
   service do
