4187728db21c4f683b10633272839fb5b27bce34
delphinus <me@delphinus.dev>
2024-01-22 12:01:03 +0900

    docs: add README and auto updating by Actions

---
+++ bin/github-generate-doc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -eu
+script=gisp
+readme=README.md
+if ! podchecker $script; then
+  exit 1
+fi
+curl -L https://cpanmin.us/ -o bin/cpanm
+chmod +x bin/cpanm
+bin/cpanm -n Pod::Markdown
+/usr/local/opt/perl/bin/pod2markdown < $script > README.md
+if git status -sb | grep -q $readme; then
+  # https://qiita.com/thaim/items/3d1a4d09ec4a7d8844ce
+  git config user.name "github-actions[bot]"
+  git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+  git add $readme
+  git commit -m "[ci skip] docs: update doc for ${script##*/}"
+  git remote set-url origin git@github.com:"$GITHUB_REPOSITORY"
+  branch=${GITHUB_REF#refs/heads/}
+  git push origin main:"$branch"
+fi
